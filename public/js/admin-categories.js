74a916df094b5842e0598c0beb772e4e39ee5076
Candela Colombo <135880086+ccolombo95@users.noreply.github.com>
2024-06-25 01:34:51 -0300

    category html

---
+++ public/js/admin-categories.js
@@ -0,0 +1,43 @@
+const categoriesContainer = document.getElementById("category-table");
+
+const template = (data) => `
+        <td>${data.title}</td>
+        <td>${data.counter}</td>
+        <td><a href="#" class="Delete" data-id="${data.id}"> X </a></td>`;
+
+const showCategories = (categories) => {
+  for (let category of categories) {
+    const tr = document.createElement("tr");
+    tr.className = "card my-2";
+    tr.innerHTML = template(category);
+    categoriesContainer.append(tr);
+  }
+};
+
+fetch("./../categories")
+  .then((res) => res.json())
+  .then((res) => showCategories(res))
+  .catch((err) => console.log(err));
+
+//! ELIMINAR/EDITAR PELICULA
+categoriesContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete")) {
+    event.preventDefault();
+    const movieId = event.target.getAttribute("data-id");
+
+    fetch(`./../categories/${movieId}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        if (response.ok) {
+          event.target.closest("tr").remove();
+        } else {
+          alert("Error al eliminar categoria.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("Error al eliminar categoria.");
+      });
+  }
+});
