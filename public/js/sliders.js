e5f74910cf495bfa075d0e03c8dc784350fbbf48
Candela Colombo <135880086+ccolombo95@users.noreply.github.com>
2024-07-10 19:36:30 -0300

    can

--- public/js/sliders.js
+++ public/js/sliders.js
@@ -1,8 +1,6 @@
 let categorizedMovies = [];
-console.log("sliders.js cargado");
 
 const cargarPeliculasSlider = (movies, category) => {
-  console.log(`Cargando slider para la categoría: ${category}`);
   const categoryContainer = document.createElement("div");
   categoryContainer.classList.add("aclamadas");
 
@@ -28,43 +26,22 @@ const cargarPeliculasSlider = (movies, category) => {
         .getElementById("aclamadasContainer")
         .appendChild(categoryContainer);
     });
-  } else {
-    console.log(
-      `No se cargó el slider para la categoría ${category} porque tiene menos de 7 películas`
-    );
   }
 };
 
 const cargarSliders = () => {
-  console.log("Cargando todos los sliders");
   categorizedMovies.forEach((category) => {
     cargarPeliculasSlider(category.movies, category.category);
   });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Documento cargado - sliders.js");
-
-  // Obtener películas para los sliders
   fetch("./../categorizedMovies")
     .then((res) => {
-      if (!res.ok) {
-        throw new Error(
-          `Error al obtener películas categorizadas: ${res.statusText}`
-        );
-      }
-      console.log("Películas categorizadas obtenidas:", res);
-      return res.json();
-    })
-    .then((res) => {
-      console.log("Películas categorizadas obtenidas en JSON:", res);
       categorizedMovies = res;
       cargarSliders();
     })
     .catch((err) => {
-      console.error("Error al obtener películas categorizadas:", err);
-      console.log(
-        "Verifica la ruta y la disponibilidad del recurso en el servidor."
-      );
+      console.log("ERROR.");
     });
 });
